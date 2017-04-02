970920f31b30cd2ad56b010bcf29b3de466eed26
Nur Mimi Afifah Binti Alihan <1526892@chester.ac.uk>
2017-04-02 19:49:24 +0800

    adding contact information and google map into contact page

--- AR-RAFFAA WEBSITE/AR-RAFFAA WEBSITE/Pages/contact.aspx.designer.cs
+++ AR-RAFFAA WEBSITE/AR-RAFFAA WEBSITE/Pages/contact.aspx.designer.cs
@@ -47,5 +47,14 @@ namespace AR_RAFFAA_WEBSITE.Pages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TxtSubject;
+        
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
     }
 }
